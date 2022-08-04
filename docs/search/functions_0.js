c136adc0d79c4cc535bdd3cfa0e47f9aca62961f
Erik Nyquist <eknyquist@gmail.com>
2022-08-04 06:00:23 -0700

    Update docs

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -4,10 +4,10 @@ var searchData=
   ['hashtable_5fcreate_1',['hashtable_create',['../hashtable__api_8h.html#a15986e05c7f0cd9f2cdc8c5b17696b61',1,'hashtable_api.h']]],
   ['hashtable_5fdefault_5fconfig_2',['hashtable_default_config',['../hashtable__api_8h.html#a007972a84ca2f222999839ed760142ba',1,'hashtable_api.h']]],
   ['hashtable_5ferror_5fmessage_3',['hashtable_error_message',['../hashtable__api_8h.html#ad965c402f8e617056d45e28a54c927ac',1,'hashtable_api.h']]],
-  ['hashtable_5fhas_5fkey_4',['hashtable_has_key',['../hashtable__api_8h.html#a847b927b76a5358071a0293d9a7da974',1,'hashtable_api.h']]],
-  ['hashtable_5finsert_5',['hashtable_insert',['../hashtable__api_8h.html#aa345f3ea2c9d8b3437c9618db4c960a3',1,'hashtable_api.h']]],
-  ['hashtable_5fnext_5fitem_6',['hashtable_next_item',['../hashtable__api_8h.html#a16d88e461d63dc7dcd1e2648bea95429',1,'hashtable_api.h']]],
-  ['hashtable_5fremove_7',['hashtable_remove',['../hashtable__api_8h.html#a6fea6e841eb193be46b00a20c0cc2e74',1,'hashtable_api.h']]],
+  ['hashtable_5fhas_5fkey_4',['hashtable_has_key',['../hashtable__api_8h.html#ae33a0f5ea3aca2454d641bf3fe5357fb',1,'hashtable_api.h']]],
+  ['hashtable_5finsert_5',['hashtable_insert',['../hashtable__api_8h.html#a7ecf50cf685ca98768a69b328f81e582',1,'hashtable_api.h']]],
+  ['hashtable_5fnext_5fitem_6',['hashtable_next_item',['../hashtable__api_8h.html#a1af5b79bf710f1a8dbe6c54ac0c544fc',1,'hashtable_api.h']]],
+  ['hashtable_5fremove_7',['hashtable_remove',['../hashtable__api_8h.html#a3a97aa30723003954d795a7a8dd4612b',1,'hashtable_api.h']]],
   ['hashtable_5freset_5fcursor_8',['hashtable_reset_cursor',['../hashtable__api_8h.html#a4e04af9382451a2c86c8e4e84f0a8224',1,'hashtable_api.h']]],
-  ['hashtable_5fretrieve_9',['hashtable_retrieve',['../hashtable__api_8h.html#aba42426892e99e958de2f919f48e6b9d',1,'hashtable_api.h']]]
+  ['hashtable_5fretrieve_9',['hashtable_retrieve',['../hashtable__api_8h.html#a45f3fe6d72ed5382addf276378966678',1,'hashtable_api.h']]]
 ];
--- docs/search/typedefs_0.js
+++ docs/search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hashtable_5fhashfunc_5ft_0',['hashtable_hashfunc_t',['../hashtable__api_8h.html#ae5b1c880a0fa4f6078447faca248b254',1,'hashtable_api.h']]]
+  ['hashtable_5fhashfunc_5ft_0',['hashtable_hashfunc_t',['../hashtable__api_8h.html#aaff1f8de1b5e4a9ddbdf88a9549e8907',1,'hashtable_api.h']]]
 ];
