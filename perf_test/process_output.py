b163113e6544e2370df7a3a12a0ca0c17b9d9979
Erik <eknyquist@gmail.com>
2022-08-07 23:15:10 -0700

    Update peformance viz graph

--- perf_test/process_output.py
+++ perf_test/process_output.py
@@ -36,23 +36,35 @@ def main():
             badkey_times.append(badkey_ns)
 
     fig, ax1 = plt.subplots()
+    fig.set_facecolor('#333333')
+    ax1.set_facecolor('#333333')
     ax2 = ax1.twinx()
 
-    ax1.set_title("28MB hashtable stress test", weight='bold')
+    ax1.set_title("28MB hashtable stress test", weight='bold', color='#ffffff')
     ax1.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
     ax2.get_yaxis().set_major_formatter(matplotlib.ticker.FormatStrFormatter('%.2f'))
 
-    ax1.set_xlabel("Number of entries in table", weight='bold')
-    ax1.set_ylabel("Nanoseconds", weight='bold')
-    ax2.set_ylabel("Load factor", color='r', weight='bold')
+    ax1.set_xlabel("Number of entries in table", weight='bold', color='#ffffff')
+    ax1.set_ylabel("Nanoseconds", weight='bold', color='#ffffff')
+    ax2.set_ylabel("Load factor", weight='bold', color='#ffffff')
 
     ax2.plot(entry_counts, load_factors, color='r', linestyle='dotted', label="Load factor")
 
     ax1.plot(entry_counts, retrieve_times, color='limegreen', label="Average item retrieval time (nanoseconds)")
     ax1.plot(entry_counts, badkey_times, color='orange', label="Average time to check for non-existent key (nanoseconds)")
-    ax1.plot(entry_counts, insert_times, color='darkviolet', label="Average item insertion time (nanoseconds)")
+    ax1.plot(entry_counts, insert_times, color='magenta', label="Average item insertion time (nanoseconds)")
     ax2.legend(bbox_to_anchor=(1,1), loc='upper right', ncol=1)
     ax1.legend(bbox_to_anchor=(0,1), loc='upper left', ncol=1)
+
+    ax2.spines['bottom'].set_color('#ffffff')
+    ax2.spines['top'].set_color('#ffffff')
+    ax2.spines['right'].set_color('#ffffff')
+    ax2.spines['left'].set_color('#ffffff')
+
+    ax1.tick_params(axis='x', colors='#ffffff')
+    ax1.tick_params(axis='y', colors='#ffffff')
+    ax2.tick_params(axis='y', colors='#ffffff')
+
     plt.show()
 
 if __name__ == "__main__":
