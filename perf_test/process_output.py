f3c6e698790265d1123937ff592aa121b7d330c3
Erik <eknyquist@gmail.com>
2022-08-07 20:43:50 -0700

    Updating docs, performance viz graph

--- perf_test/process_output.py
+++ perf_test/process_output.py
@@ -38,17 +38,19 @@ def main():
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
+    ax1.set_title("28MB hashtable stress test", weight='bold')
     ax1.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
     ax2.get_yaxis().set_major_formatter(matplotlib.ticker.FormatStrFormatter('%.2f'))
 
-    ax1.set_xlabel("No. of entries in table")
-    ax1.set_ylabel("Nanoseconds")
-    ax2.set_ylabel("Load factor", color='r')
+    ax1.set_xlabel("Number of entries in table", weight='bold')
+    ax1.set_ylabel("Nanoseconds", weight='bold')
+    ax2.set_ylabel("Load factor", color='r', weight='bold')
 
     ax2.plot(entry_counts, load_factors, color='r', linestyle='dotted', label="Load factor")
-    ax1.plot(entry_counts, insert_times, color='orange', label="Avg. insert time")
-    ax1.plot(entry_counts, retrieve_times, color='limegreen', label="Avg. retrieve time")
-    ax1.plot(entry_counts, badkey_times, color='darkviolet', label="Avg. time to check for non-existent key")
+
+    ax1.plot(entry_counts, retrieve_times, color='limegreen', label="Average item retrieval time (nanoseconds)")
+    ax1.plot(entry_counts, badkey_times, color='orange', label="Average time to check for non-existent key (nanoseconds)")
+    ax1.plot(entry_counts, insert_times, color='darkviolet', label="Average item insertion time (nanoseconds)")
     ax2.legend(bbox_to_anchor=(1,1), loc='upper right', ncol=1)
     ax1.legend(bbox_to_anchor=(0,1), loc='upper left', ncol=1)
     plt.show()
